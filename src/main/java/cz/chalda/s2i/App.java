71798fc6e8168ab76bce0aeb18f0c39fbc3b20d0
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-09 11:02:38 +0100

    git init

---
+++ src/main/java/cz/chalda/s2i/App.java
@@ -0,0 +1,13 @@
+package cz.chalda.s2i;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
