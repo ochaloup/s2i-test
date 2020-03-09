31e487a13f860dba443f5b511ec12567a757321d
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-09 14:07:53 +0100

    creating hello s2i with main class

--- src/main/java/cz/chalda/s2i/HelloS2I.java
+++ src/main/java/cz/chalda/s2i/HelloS2I.java
@@ -4,10 +4,10 @@ package cz.chalda.s2i;
  * Hello world!
  *
  */
-public class App 
+public class HelloS2I
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello S2I!" );
     }
 }
