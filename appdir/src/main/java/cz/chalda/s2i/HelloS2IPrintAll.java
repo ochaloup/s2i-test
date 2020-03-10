215e8bba85eb7fe2900b868abc5cec8ae397fa95
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-10 11:25:52 +0100

    .s2i directory added

--- appdir/src/main/java/cz/chalda/s2i/HelloS2IPrintAll.java
+++ appdir/src/main/java/cz/chalda/s2i/HelloS2IPrintAll.java
@@ -8,7 +8,5 @@ public class HelloS2IPrintAll {
         System.out.println("Hello S2I printing");
         System.out.println("Environmental Variables");
         System.getenv().forEach((k, v) -> System.out.println("Key: " + k + ", Value: " + v));
-        System.out.printf("%nSystem Properties%n");
-        System.getProperties().forEach((pn, pv) -> System.out.println("Name: " + pn + ", Value: " + pv));
     }
 }
